fc7a6b17d0dd5d7820153926923dbe8879cb6db4
Paulo Henrique <paulohfprates@gmil.com>
2019-07-10 13:43:29 -0300

    Passport configuraçoes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use App\User;
+use Illuminate\Support\Facades\Validator;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,10 +14,51 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('teste', function (Request $request) {
-    return "Ola Mundo";
+Route::post('cadastro', function (Request $request) {
+    $data = $request->all();
+
+    $validacao = Validator::make($data, [
+        'name' => ['required', 'string', 'max:255'],
+        'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+        'password' => ['required', 'string', 'min:8', 'confirmed'],
+    ]);
+
+    if($validacao->fails()){
+        return $validacao->errors();
+    }
+    $user = User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => Hash::make($data['password']),
+    ]);
+
+    $user->token = $user->createToken($user->email)->accessToken;
+    return $user;
 });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::post('login', function (Request $request) {
+    $data = $request->all();
+
+    $validacao = Validator::make($data, [
+        'email' => ['required', 'string', 'email', 'max:255'],
+        'password' => ['required', 'string'],
+    ]);
+
+    if($validacao->fails()){
+        return $validacao->errors();
+    }
+
+    if(Auth::attempt(['email'=>$data['email'],'password'=>$data['password']])){
+        $user = auth()->user();
+        $user->token = $user->createToken($user->email)->accessToken;
+        return $user;
+    }else{
+        return "erro";
+    }
+    
+    
+});
+
+Route::middleware('auth:api')->get('/usuario', function (Request $request) {
     return $request->user();
 });
